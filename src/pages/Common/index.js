c5e3e927bbbd515aad82ff9a5f64cd6de7c3603f
TD21forever <435212619@qq.com>
2021-09-01 15:49:23 +0800

    update

--- src/pages/Common/index.js
+++ src/pages/Common/index.js
@@ -1,19 +1,100 @@
 import React from 'react'
 import './dist/index.css'
-import {Link} from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
-export default class MainHeader extends React.Component {
+class MainHeader extends React.Component {
+    renderButton = () => {
+        if (this.props.location.pathname === "/history")
+            return (<li className="login-box">
+                <button className="delete-button" onClick={this.handleOnclick}>删除历史</button>
+            </li>)
+        return (
+            <>
+                <li className="search-box"><input type="text" className="search" /></li>
+                <li className="login-box"><button className="login-button">登录</button></li>
+            </>
+        )
+    }
+    handleOnclick = () => {
+        if (window.localStorage.hasOwnProperty("historyID")) {
+
+            delete window.localStorage.historyID
+            this.props.history.push('/')
+
+        }
+        else
+            alert("当前没有历史记录～")
+    }
     render() {
         return (
             <header className="header-container">
                 <Link to="/" className="logo"></Link>
                 <ul className="item-list">
                     <li className="title-box"><Link to="/" className="title">首页</Link></li>
-                    <li className="search-box"><input type="text" className="search"/></li>
-                    <li className="login-box"><button className="login-button">登录</button></li>
+                    {this.renderButton()}
                 </ul>
 
             </header>
         )
     }
 }
+
+class BackToTop extends React.Component {
+
+    state = {
+        show:false,
+        memScrollTop:0
+    }
+
+    backToTop = () => {
+
+        let scrollTop = document.documentElement.scrollTop
+        var subH = parseInt(scrollTop / 50);
+        var timer = setInterval(function(){
+            scrollTop -= subH;
+            if(scrollTop <= 0){
+                document.documentElement.scrollTop = 0;
+                clearInterval(timer);
+            }else{
+                document.documentElement.scrollTop = scrollTop;
+            }
+        },1)    
+
+    }
+
+    componentDidMount(){
+        window.addEventListener("scroll",() => {
+
+            let scrollTop = document.documentElement.scrollTop
+
+            if(scrollTop > 500 && scrollTop-this.state.memScrollTop > 0)
+                this.setState({show:true,memScrollTop:scrollTop})
+            else
+                this.setState({show:false,memScrollTop:scrollTop})
+
+        })
+    }
+  
+
+    renderEle = () => {
+        if (this.state.show) {
+            return (
+                <div className="top" onClick={this.backToTop}>
+                    <i className="iconfont icon-top"></i>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <>
+            {this.renderEle()}
+            </>
+
+        )
+    }
+}
+
+export default withRouter(MainHeader);
+export { BackToTop }
--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -28,10 +28,24 @@ class History extends Component {
         this.getArticle()
     }
 
+    renderNoHistory = ()=>{
+        console.log(this.state.articleInfo.length === 0);
+        if(this.state.articleInfo.length === 0)
+            return (
+
+                <div className="no-history">
+
+                    <h3> 没有历史记录，快去浏览哦～～～ </h3>
+
+                </div>
+
+            )
+    }
 
     render() {
         return (
             <div className="history-container">
+                { this.renderNoHistory() }
                 {this.state.articleInfo.map(item => (<Post article={item}></Post>))}
             </div> 
             )
